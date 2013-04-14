73d7a674d867b67a58efb6c91e15f626f7d0b078
Matt Sweetman <git@webroo.org>
2013-04-14 13:55:49 +0100

    Initial commit. Basic setup of the Haggerston-generated site.

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    clean: ['out/'],
+    copy: {
+      assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/',
+            src: ['**'],
+            dest: 'out/'
+          }
+        ]
+      }
+    },
+    stylus: {
+      site: {
+        files: {
+          'out/styles/styles.css': 'src/styles/styles.stylus'
+        }
+      }
+    },
+    haggerston: {
+      options: {
+        contentPath: 'src/content',
+        templatesPath: 'src/templates',
+        dest: 'out'
+      }
+    },
+    watch: {
+      haggerston: {
+        files: [
+          'src/content/**/*.json',
+          'src/content/**/*.md',
+          'src/templates/**/*.html'
+        ],
+        tasks: 'haggerston'
+      },
+      stylus: {
+        files: [
+          'src/styles/**/*.stylus'
+        ],
+        tasks: 'stylus'
+      },
+      assets: {
+        files: [
+          'assets/**'
+        ],
+        tasks: 'copy'
+      }
+    },
+    connect: {
+      site: {
+        options: {
+          base: 'out/'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-haggerston');
+
+  grunt.registerTask('build', ['clean', 'copy', 'stylus', 'haggerston']);
+  grunt.registerTask('serve', ['build', 'connect', 'watch']);
+  
+  grunt.registerTask('default', ['build']);
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# webroo.org
+
+After cloning the repository:
+    
+    npm install
+    grunt
+    
+To serve the generated site via localhost and update automatically as you edit files:
+
+    grunt serve
+
+===
+    
+Copyright (c) 2013 Matt Sweetman
